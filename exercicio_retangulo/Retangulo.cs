edf06395bc1e80ff08676ec7a950441d4fedabaf
damarysali1405-pixel <damarysali1405@gmail.com>
2026-05-05 08:57:33 -0300

    Implement Retangulo class with area and perimeter methods

---
+++ exercicio_retangulo/Retangulo.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damarys_Aline_2
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Largura * Largura);
+        }
+    }
+}
